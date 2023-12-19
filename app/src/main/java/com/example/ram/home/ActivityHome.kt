32445865406d8fa4f56e6507b91f5d686dc05ebd
MarasiganKenth <kepa.marasigan.up@phinmaed.com>
2023-12-19 15:58:21 +0800

    Responsive Login Responsive Home Responsive Appointment Card Added fonts Added Delete icon Added Edit icon alphagradient Removed activiity home Removed authentication Removed item list

--- app/src/main/java/com/example/ram/home/ActivityHome.kt
+++ app/src/main/java/com/example/ram/home/ActivityHome.kt
@@ -2,10 +2,9 @@ package com.example.ram.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ram.R
 import com.example.ram.databinding.ActivityActiviityHomeBinding
 
-class ActiviityHome : AppCompatActivity() {
+class ActivityHome : AppCompatActivity() {
     private lateinit var binding: ActivityActiviityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
