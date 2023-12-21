512938d5d33978e70c62bb254c0c3e39a9172a09
LichtBlade <risi.bustillo.up@phinmaed.com>
2023-12-22 02:05:16 +0800

    updated

--- app/src/main/java/com/example/ram/MainActivity.kt
+++ app/src/main/java/com/example/ram/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ram.authentication.Authentication
 import com.example.ram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.btnStudent.setOnClickListener {
+            val intent = Intent(this,Authentication::class.java)
+            startActivity(intent)
+        }
     }
 }
