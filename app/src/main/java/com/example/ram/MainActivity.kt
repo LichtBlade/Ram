062bf0cee53fc47f2f758b75abc5a76d77fc96d0
MarasiganKenth <kepa.marasigan.up@phinmaed.com>
2023-12-22 02:16:02 +0800

    created AppointmentPurpose layout for phone and tablet

--- app/src/main/java/com/example/ram/MainActivity.kt
+++ app/src/main/java/com/example/ram/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ram.databinding.ActivityMainBinding
-import com.example.ram.home.ActivityHome
+import com.example.ram.ActivityHome
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
