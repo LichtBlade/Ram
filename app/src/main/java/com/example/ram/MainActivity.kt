f8df85cc4ca965b6e9a0631e130eaed68c8e8dd7
LichtBlade <risi.bustillo.up@phinmaed.com>
2023-12-22 01:18:18 +0800

    updated

--- app/src/main/java/com/example/ram/MainActivity.kt
+++ app/src/main/java/com/example/ram/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ram.databinding.ActivityMainBinding
+import com.example.ram.home.ActivityHome
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,5 +13,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnEditprofileConfirm.setOnClickListener {
+            if(validation()){
+                startActivity(Intent(this,ActivityHome::class.java))
+                finish()
+            }
+        }
+    }
+
+    private fun validation():Boolean{
+        val student = binding.etStudentID?.text.toString()
+        val password = binding.etPassword?.text.toString()
+
+        if(student.isEmpty()){
+            binding.etStudentID?.error = "Please fill up"
+            return false
+        }
+
+        if (password.isEmpty()){
+            binding.etPassword?.error = "Please fill up"
+            return false
+        }
+        return true
     }
 }
