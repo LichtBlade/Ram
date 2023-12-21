f88346d02316ed909b38a9dec77703122e8bf5ea
LichtBlade <risi.bustillo.up@phinmaed.com>
2023-12-22 01:33:39 +0800

    updated

--- app/src/main/java/com/example/ram/home/ActivityHome.kt
+++ app/src/main/java/com/example/ram/home/ActivityHome.kt
@@ -2,14 +2,13 @@ package com.example.ram.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ram.databinding.ActivityHomeBinding
-
+import com.example.ram.databinding.ActivityActiviityHomeBinding
 
 class ActivityHome : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityActiviityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityActiviityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
