06ddc3f56352b7ed66c7711ca23abee6411b11f3
LichtBlade <risi.bustillo.up@phinmaed.com>
2023-12-22 13:16:19 +0800

    updated

--- app/src/main/java/com/example/ram/homepage/ActivityHome.kt
+++ app/src/main/java/com/example/ram/homepage/ActivityHome.kt
@@ -1,11 +1,14 @@
-package com.example.ram
+package com.example.ram.homepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ram.databinding.ActivityHomeBinding
 
 class ActivityHome : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var newArrayList: ArrayList<DataOfAppointmentCard>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
---
+++ app/src/main/java/com/example/ram/homepage/DataOfAppointmentCard.kt
@@ -0,0 +1,8 @@
+package com.example.ram.homepage
+
+data class DataOfAppointmentCard(
+    val referenceId: String? = null,
+    val scheduleDate: String? = null,
+    val scheduleTime: String? = null,
+    val purpose: String? = null,
+    val status: String?=null)
