e60367393c43607ebc58debe3cdb0722d78b4d31
LichtBlade <risi.bustillo.up@phinmaed.com>
2024-02-06 23:23:54 +0800

    RecyclerView sample

--- app/src/main/java/com/example/ram/homepage/ActivityHome.kt
+++ app/src/main/java/com/example/ram/homepage/ActivityHome.kt
@@ -2,16 +2,83 @@ package com.example.ram.homepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ram.R
 import com.example.ram.databinding.ActivityHomeBinding
 
 class ActivityHome : AppCompatActivity() {
+    // For Binding
     private lateinit var binding: ActivityHomeBinding
+
+    //for RecyclerView
     private lateinit var newRecyclerView: RecyclerView
     private lateinit var newArrayList: ArrayList<DataOfAppointmentCard>
+
+    // Array for sample
+    lateinit var referenceId : Array<String>
+    lateinit var scheduleDate : Array<String>
+    lateinit var scheduleTime : Array<String>
+    lateinit var purpose : Array<String>
+    lateinit var status : Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        referenceId = arrayOf(
+            "1",
+            "2",
+            "3"
+        )
+
+        scheduleDate = arrayOf(
+            "March 23",
+            "September 6",
+            "January 2"
+        )
+
+        scheduleTime = arrayOf(
+            "11:11",
+            "10:10",
+            "9:09"
+        )
+
+        purpose = arrayOf(
+            "Grades",
+            "Card",
+            "Id"
+        )
+
+        status = arrayOf(
+            "ready",
+            "secret",
+            "not yet"
+        )
+
+        newRecyclerView=findViewById(R.id.recyclerView)
+        newRecyclerView.layoutManager = LinearLayoutManager(this)
+        newRecyclerView.setHasFixedSize(true)
+
+        newArrayList = arrayListOf<DataOfAppointmentCard>()
+        getUserData()
     }
+
+    private fun getUserData() {
+        for (i in referenceId.indices) {
+            val dataOfAppointmentCard = DataOfAppointmentCard(
+                referenceId[i],
+                scheduleDate[i],
+                scheduleTime[i],
+                purpose[i],
+                status[i]
+            )
+            newArrayList.add(dataOfAppointmentCard)
+        }
+        newRecyclerView.adapter = MyAdapter(newArrayList)
+    }
+
+
+
 }
