490b4af8e4b6b63956e6d704df77c8cb8f617637
Alexander <jobe.vargas.up@phinmaed.com>
2024-02-12 22:37:47 +0800

    added binding to activity details/schedule/reference

--- app/src/main/java/com/example/ram/activity_details.kt
+++ app/src/main/java/com/example/ram/activity_details.kt
@@ -2,10 +2,14 @@ package com.example.ram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ram.databinding.ActivityDetailsBinding
+import com.example.ram.databinding.ActivityReferenceBinding
 
 class activity_details : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/ram/activity_reference.kt
+++ app/src/main/java/com/example/ram/activity_reference.kt
@@ -2,10 +2,15 @@ package com.example.ram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ram.databinding.ActivityReferenceBinding
+import com.example.ram.databinding.ActivityScheduleBinding
 
 class activity_reference : AppCompatActivity() {
+    private lateinit var binding: ActivityReferenceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reference)
+        binding = ActivityReferenceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
