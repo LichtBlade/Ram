64393eafb8325e250163bbeaf1244a1783420a07
Alexander <jobe.vargas.up@phinmaed.com>
2024-02-27 14:28:15 +0800

    modified help screen

--- app/src/main/java/com/example/ram/helppage/HelpScreen.kt
+++ app/src/main/java/com/example/ram/helppage/HelpScreen.kt
@@ -27,8 +27,44 @@ class HelpScreen : AppCompatActivity() {
     private fun addDataToList() {
         mList.add(
             DataOfHelpCard(
-                "How do i login?",
-                "Secret kaya mo na yan geng geng"
+                "How do I login?",
+                "You can login using your Student ID Number. You can get your Password from the ITS office inside the campus."
+            )
+        )
+
+
+        mList.add(
+            DataOfHelpCard(
+                "What documents or information should i bring to my appointment?",
+                "When creating an appointment details such as required documents and fees will be displayed. Every school Documents have different requirements."
+            )
+        )
+
+        mList.add(
+            DataOfHelpCard(
+                "Can i reschedule or cancel my appointment?",
+                "Yes, You can edit or reschedule and delete appointments inside the Home Page where your appointments are displayed."
+            )
+        )
+
+        mList.add(
+            DataOfHelpCard(
+                "What is the policy for missed or late appointments?",
+                "Your appointment will be marked as 'No show' if you miss an appointment or if you are late for your appointment."
+            )
+        )
+
+        mList.add(
+            DataOfHelpCard(
+                "Is there a limit to the number of appointments i can schedule?",
+                "There is no limited number of appointments. You can book multiple appointments with different purposes."
+            )
+        )
+
+        mList.add(
+            DataOfHelpCard(
+                "Can a representative attend to my appointment?",
+                "Yes! You can set an appointment for them, Just make sure to provide an authorization letter during appointment period."
             )
         )
     }
